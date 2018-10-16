5f550daf5d005dfb0913f69c500c0b985f440d99
connormcd22 <connor.mcdonough28@gmail.com>
2018-10-16 08:28:36 -0600

    hw

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app     = express();
 const Pokemon = require('./models/pokemon.js');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 app.use('/css', express.static(__dirname + '/css'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
 
 
 
@@ -11,8 +16,7 @@ app.get('/', (req, res) => {
 
 
 
-// Index route
-// Shows all the fruits
+
 app.get('/pokemon', (req, res) => {
   res.render("index.ejs", {
 
@@ -20,6 +24,17 @@ app.get('/pokemon', (req, res) => {
 
   });
 });
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+app.post('/pokemon', (req, res) => {
+
+
+  Pokemon.push(req.body);
+
+  res.redirect('/pokemon')
+});
+
 app.get('/pokemon/:index', (req, res) => {
   res.render('show.ejs', {
 
@@ -30,6 +45,25 @@ app.get('/pokemon/:index', (req, res) => {
 
 
 
+
+
+app.get('/pokemon/:id/edit', (req, res) => {
+  res.render('edit.ejs', {
+    pokemon: Pokemon[req.params.id],
+    id: req.params.id
+  });
+});
+app.delete('/pokemon/:id', (req, res) => {
+  Pokemon.splice(req.params.id, 1);
+  res.redirect('/pokemon');
+});
+
+app.put('/pokemon/:id', (req, res) => {
+
+  Pokemon[req.params.id] = req.body;
+
+  res.redirect('/pokemon')
+})
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
